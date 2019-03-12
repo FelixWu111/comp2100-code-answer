87af85faabfa0772c9cb97612f6ec436c5a6064c
Duy Khuu <u6380923@anu.edu.au>
2019-03-12 19:43:59 +1100

    Update RBTree

--- activity_02/part2/RBTree.java
+++ activity_02/part2/RBTree.java
@@ -1,44 +1,183 @@
 public class RBTree<T extends Comparable<T>> {
-    private Node<T> _r;
+    private Node<T> root; // The root node of the tree
 
     public class Node<T extends Comparable<T>> {
-        boolean c;          // Color of the node
-        T v;                // Value
-        Node<T> l, r, p;    // Children
+        Colour colour;       // Node colour
+        T value;             // Node value
+        Node<T> parent;      // Parent node
+        Node<T> l, r;        // Children nodes
 
+        // Value node
         public Node(T value) {
-            v = value;
-            c = true;
-            p = l = r = null;
+            this.value = value;
+            this.colour = Colour.RED;
+            this.parent = null;
+
+            // Initialise children leaf nodes
+            this.l = new Node<>();
+            this.r = new Node<>();
+            this.l.parent = this;
+            this.r.parent = this;
         }
 
-        public T getKey() {
-            return v;
+        // Leaf node
+        public Node() {
+            this.value = null;
+            this.colour = Colour.BLACK;
         }
+
+        // Insert node into tree, ignoring colour
+        public void insert(Node<T> n) {
+            // TODO: Implement this (1/3)
+
+            // NOTE: To make implementing the RBTree easier, instead of having null
+            // children, nodes with a null value are used instead so take this into
+            // account if you are trying to copy over your part 1 solution
+
+            // HINT: Remember to update the parent of the node being inserted!
+
+
+        }
+
+        // Rotate the node so it becomes the child of its right branch
+        /*
+            e.g.
+                  [x]                    b
+                 /   \                 /   \     
+               a       b     == >   [x]     f    
+              / \     / \           /  \      
+             c  d    e   f         a    e
+                                  / \
+                                 c   d
+        */
+        public void rotateLeft() {
+            // Make parent (if it exists) and right branch point to each other
+            if (parent != null) {
+                // Determine whether this node is the left or right child of its parent
+                if (parent.l.value == value) {
+                    parent.l = r;
+                } else {
+                    parent.r = r;
+                }
+            }
+            r.parent = parent;
+
+            parent = r;
+            // Take right node's left branch
+            r = parent.l;
+            r.parent = this;
+            // Take the place of the right node's left branch
+            parent.l = this;
+        }
+
+        // Rotate the node so it becomes the child of its left branch
+        /*
+            e.g.
+                  [x]                    a
+                 /   \                 /   \     
+               a       b     == >     c     [x]   
+              / \     / \                   /  \      
+             c  d    e   f                 d    b
+                                               / \
+                                              e   f
+        */
+        public void rotateRight() {
+            // TODO: Implement this function (2/3)
+            // HINT: It is the mirrored version of rotateLeft()
+        }
+
     }
 
+    // Initialise empty RBTree
     public RBTree() {
-        _r=null;
+        root = null;
     }
-    
+
+
+    // Insert node into RBTree
+    private void insert(Node<T> x) {
+        // TODO: Complete this function (3/3)
+        // You need only finish cases 1, 2 and 3; the rest has been done for you
+ 
+        // Insert node into tree
+        if (root == null) {
+            root = x;
+        } else {
+            root.insert(x);
+        }
+
+        // Fix tree
+        while (x.value != root.value && x.parent.colour == Colour.RED) {
+            boolean left = x.parent == x.parent.parent.l; // Is parent a left node
+            Node<T> y = left ? x.parent.parent.r : x.parent.parent.l; // Get opposite "uncle" node to parent
+
+            if (y.colour == Colour.RED) {
+                // Case 1: Recolour
+                // TODO: Implement this
+
+                // Check if violated further up the tree
+                x = x.parent.parent;
+            } else {
+                if (x.value == (left ? x.parent.r.value : x.parent.l.value)) {
+                    // Case 2 : Left (uncle is left node) / Right (uncle is right node) Rotation
+                    x = x.parent;
+                    if (left) {
+                        // Perform left rotation
+                        if (x.value == root.value) root = x.r; // Update root
+                        x.rotateLeft();
+                    } else {
+                        // Perform right rotation
+                        // TODO: Implement this
+                    }
+
+                }
+                // Adjust colours to ensure correctness after rotation
+                x.parent.colour = Colour.BLACK;
+                x.parent.parent.colour = Colour.RED;
+                // Case 3 : Right (uncle is left node) / Left (uncle is right node) Rotation
+                // TODO: Complete this
+                    if (left) {
+                        // Perform right rotation
+                    } else
+                    {
+                        // Perform left rotation
+                    }
+            }
+        }
+
+        // Ensure property 2 (root and leaves are black) holds
+        root.colour = Colour.BLACK;
+    }
+
+
+    // Demo functions
+    // (Safely) insert a value into the tree
+    public void insert(T value) {
+        Node<T> node = new Node<T>(value);
+        if (node != null)
+            insert(node);
+    }
+
+    // Return the result of a pre-order transversal of the tree
     private void preOrder(Node<T> tree) {
-        if(tree != null) {
-            System.out.print(tree.v +" ");
+        if(tree != null && tree.value != null) {
+            System.out.print(tree.value +" ");
             preOrder(tree.l);
             preOrder(tree.r);
         }
     }
-    
+
     public void preOrder() {
-        preOrder(_r);
+        preOrder(root);
         System.out.println();
     }
-    
-    private Node<T> find(Node<T> x, T v) {
-        if (x==null)
-            return x;
 
-        int cmp = v.compareTo(x.v);
+    // Return the corresponding node of a value, if it exists in the tree
+    private Node<T> find(Node<T> x, T v) {
+        if (x.value == null)
+            return null;
+
+        int cmp = v.compareTo(x.value);
         if (cmp < 0)
             return find(x.l, v);
         else if (cmp > 0)
@@ -48,16 +187,13 @@ public class RBTree<T extends Comparable<T>> {
     }
 
     public Node<T> search(T key) {
-        return find(_r, key);
+        return find(root, key);
     }
 
-    private void insert(Node<T> n) {
-        //!TODO: Add your code here.
+
+    public enum Colour {
+        RED,
+        BLACK;
     }
-    
-    public void insert(T key) {
-        Node<T> node = new Node<T>(key);
-        if (node != null)
-            insert(node);
-    }
+
 }
